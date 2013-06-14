0dcc5249f61e45e3f4c64ed170167a82bcfdf754
Martin Jørgensen <martinnjorgensen@gmail.com>
2013-06-15 00:42:10 +0200

    Indlæser sange til intern dict.

---
+++ main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+import sys
+import re
+import os
+
+# Lookup ordbøger
+titelTilFil = {}
+filTilInfo = {}
+
+# regexp der fanger sangmakroen og giver en tuple af strenge.
+# jf. \sang makroen er outputet (titel,kunstner,melodi,tekst)
+regex = re.compile('sang\{(.*)\}.\{(.*)\}.\{(.*)\}.\{(.*)\}.',re.DOTALL)
+
+def hoved():
+    args = sys.argv
+    for fil in os.listdir("./sange"):
+        if fil.endswith(".tex"):
+            analyserFil(fil)
+    print filTilInfo["blivpaadikubaby.tex"]
+            
+def analyserFil(filnavn):
+    fil = open("./sange/" + filnavn,"r")
+    smidIDict(filnavn,fortolkSang(fil.read()))
+    fil.close()
+
+def smidIDict(filnavn, info):
+    filTilInfo[filnavn] = info
+    titelTilFil[info[0]] = filnavn
+
+def fortolkSang(streng):
+    data = re.findall(regex,streng)
+    return data[0]
+ 
+
+if __name__ == "__main__":
+    hoved()
